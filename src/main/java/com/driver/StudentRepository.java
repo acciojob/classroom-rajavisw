3f07b2a96338feb6cfefe34810c1150860547646
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-05-25 13:29:29 +0530

    first commit

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,79 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.getName(),student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.getName(), teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            teacherStudentMapping.get(teacher).add(student);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        Student stud=null;
+        for(String s:studentMap.keySet()){
+            if(s.equals(student)){
+                stud=studentMap.get(s);
+                break;
+            }
+        }
+        return stud;
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        Teacher techr=null;
+        for(String t:teacherMap.keySet()){
+            if(t.equals(teacher)){
+                techr=teacherMap.get(t);
+                break;
+            }
+        }
+        return techr;
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        List<String> students=new ArrayList<>();
+        for(String t:teacherStudentMapping.keySet()){
+            if(t.equals(teacher)){
+                return teacherStudentMapping.get(t);
+            }
+        }
+        return students;
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> studs=new ArrayList<>();
+        for(String s:studentMap.keySet()){
+            studs.add(s);
+        }
+        return studs;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        for(String t:teacherMap.keySet()){
+            if(t.equals(teacher)){
+                teacherMap.remove(t);
+            }
+        }
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        for(String t:teacherMap.keySet()){
+            teacherMap.remove(t);
+        }
     }
 }
